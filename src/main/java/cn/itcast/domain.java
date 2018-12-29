347493a27bfad6c23d3772d43642ed7079565fcf
root <1085854776@qq.com>
2018-12-29 20:17:45 +0800

    ss

--- src/main/java/cn/itcast/domain.java
+++ src/main/java/cn/itcast/domain.java
@@ -2,7 +2,7 @@ package cn.itcast;
 
 public class domain {
     public static void main(String[] args) {
-        System.out.println("55555");
+
 
          System.out.println("66666");
         System.out.println("7777");
