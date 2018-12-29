8e97b298cfa0f33e2940f51bf57ed68720529d48
root <1085854776@qq.com>
2018-12-29 20:12:27 +0800

    aa

--- src/main/java/cn/itcast/domain.java
+++ src/main/java/cn/itcast/domain.java
@@ -2,9 +2,8 @@ package cn.itcast;
 
 public class domain {
     public static void main(String[] args) {
-        System.out.println("55555");
 
         System.out.println("66666");
-        System.out.println("7777");
+
     }
 }
