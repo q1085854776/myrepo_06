7c44ce7374fda242b719ec1570f1a9072b4f6d66
root <1085854776@qq.com>
2018-12-29 20:15:08 +0800

    aa

--- src/main/java/cn/itcast/domain.java
+++ src/main/java/cn/itcast/domain.java
@@ -4,7 +4,7 @@ public class domain {
     public static void main(String[] args) {
         System.out.println("55555");
 
-        System.out.println("66666");
+         System.out.println("66666");
 
     }
 }
